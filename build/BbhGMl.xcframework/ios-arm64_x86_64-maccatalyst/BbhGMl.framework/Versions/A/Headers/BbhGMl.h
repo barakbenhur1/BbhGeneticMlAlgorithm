12663622f29f458abd196f13579b3984f9c2b77d
barak ben hur <barakbenhur@gmail.com>
2021-06-24 02:07:43 +0300

    first commit

---
+++ build/BbhGMl.xcframework/ios-arm64_x86_64-maccatalyst/BbhGMl.framework/Versions/A/Headers/BbhGMl.h
@@ -0,0 +1,18 @@
+//
+//  BbhGMl.h
+//  BbhGMl
+//
+//  Created by Interactech on 24/06/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BbhGMl.
+FOUNDATION_EXPORT double BbhGMlVersionNumber;
+
+//! Project version string for BbhGMl.
+FOUNDATION_EXPORT const unsigned char BbhGMlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BbhGMl/PublicHeader.h>
+
+
